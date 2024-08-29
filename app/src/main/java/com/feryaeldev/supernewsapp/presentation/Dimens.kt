6af18b91a8155a8281352efe605d468fad6d99cb
FeryaelJustice <nano9gs@hotmail.es>
2024-08-29 14:46:28 +0200

    onboarding

---
+++ app/src/main/java/com/feryaeldev/supernewsapp/presentation/Dimens.kt
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2024. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.feryaeldev.supernewsapp.presentation
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+    val MediumPadding1 = 24.dp
+    val MediumPadding2 = 30.dp
+    val IndicatorSize = 14.dp
+
+}
---
+++ app/src/main/java/com/feryaeldev/supernewsapp/presentation/onboarding/OnBoardingScreen.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2024. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.feryaeldev.supernewsapp.presentation.onboarding
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import com.feryaeldev.supernewsapp.presentation.onboarding.components.OnBoardingPage
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun OnBoardingScreen(innerPadding: PaddingValues) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        val pagerState = rememberPagerState(initialPage = 0) {
+            pages.size
+        }
+        val buttonState = remember {
+            derivedStateOf {
+                when (pagerState.currentPage) {
+                    0 -> listOf("", "Next")
+                    1 -> listOf("Back", "Next")
+                    2 -> listOf("Back", "Get Started")
+                    else -> listOf("", "")
+                }
+            }
+        }
+
+        HorizontalPager(state = pagerState) { index ->
+            OnBoardingPage(page = pages[index], modifier = Modifier.fillMaxSize())
+        }
+    }
+}
