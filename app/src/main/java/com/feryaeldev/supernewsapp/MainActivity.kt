1485046ac47bbf6186dcc2e9a4d6472cc60f3a5c
FeryaelJustice <nano9gs@hotmail.es>
2024-09-11 16:34:07 +0200

    onboarding

--- app/src/main/java/com/feryaeldev/supernewsapp/MainActivity.kt
+++ app/src/main/java/com/feryaeldev/supernewsapp/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import com.feryaeldev.supernewsapp.presentation.onboarding.OnBoardingScreen
 import com.feryaeldev.supernewsapp.presentation.onboarding.components.OnBoardingPage
 import com.feryaeldev.supernewsapp.ui.theme.SuperNewsAppTheme
@@ -22,6 +23,7 @@ import com.feryaeldev.supernewsapp.ui.theme.SuperNewsAppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         installSplashScreen()
         enableEdgeToEdge()
         setContent {
--- app/src/main/java/com/feryaeldev/supernewsapp/presentation/Dimens.kt
+++ app/src/main/java/com/feryaeldev/supernewsapp/presentation/Dimens.kt
@@ -15,4 +15,5 @@ object Dimens {
     val MediumPadding2 = 30.dp
     val IndicatorSize = 14.dp
 
+    val PageIndicatorWidth = 52.dp
 }
