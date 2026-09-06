21d7bc32e52f8be6f243cd87d0538b69e8122ed5
FeryaelJustice <nano9gs@hotmail.es>
2026-09-06 14:25:56 +0200

    build: update Gradle wrapper to 9.7.1 and bump dependency versions
    - Update Gradle distribution wrapper to version 9.7.1
    - Bump Compose BOM version to 2026.05.01
    - Bump Navigation 3 version to 1.1.2
    - Bump Compose Foundation version to 1.11.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.feryaeljustice.supernewsapp"
         minSdk = 35
         targetSdk = 37
-        versionCode = 15
-        versionName = "2.1.0"
+        versionCode = 16
+        versionName = "2.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
